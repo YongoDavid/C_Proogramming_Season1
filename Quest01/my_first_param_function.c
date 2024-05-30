c7d3518e4524ffde4b5464436d2ac076caa4101c
Yongo David <davidyongo9@gmail.com>
2024-05-30 01:35:12 +0100

    C program update

---
+++ Quest01/my_first_param_function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// function will printf("detonation in... %d seconds.\n", seconds_left);
+
+  
+void detonation_in(){
+    int timer = 10;
+   
+    while (timer <= 10) {
+    int seconds_left = timer-1;
+    printf("detonation in... %d seconds.\n", seconds_left);
+    return detonation_in++ ;
+  };
+
+  int main() {
+  timer = 10;
+
+  while (XX) {
+    detonation_in(timer);
+    XX
+  }
+  return 0;
+}
+}
